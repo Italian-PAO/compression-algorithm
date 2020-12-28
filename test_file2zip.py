82bead0eff279a40f7ab920ab716f84583309dc0
Italian-PAO <1410430449@qq.com>
2020-12-28 22:53:47 +0800

    final version

--- test_file2zip.py
+++ test_file2zip.py
@@ -1,2 +1,7 @@
-import Zip
+from Zip import files2zip
 
+file_path = input("请输入要压缩的文件路径：")
+
+info = files2zip.files2zip(file_path)
+
+print(info)
---
+++ test_zip2files.py
@@ -0,0 +1,7 @@
+from Zip import zip2files
+
+zip_path = input("请输入要解压的文件路径：")
+
+info = zip2files.zip2files(zip_path)
+
+print(info)
